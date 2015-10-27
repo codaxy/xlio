63fe919334cbddfabe783905f6a06890987054c3
Bajic Dusko <bajicdusko@gmail.com>
2015-10-27 10:25:38 +0100

    white space reservation

---
+++ Source/Codaxy.Xlio/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Codaxy.Xlio
+{
+    namespace Model.Oxml
+    {
+        public partial class CT_Rst
+        {
+            [XmlAttribute("xml:space")]
+            public String Space
+            {
+                get; set;
+            }
+        }
+    }
+}
